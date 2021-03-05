16de922d04202e08f05c109e732d497aaf7d1755
Tannaz <51849333+tannazvhd@users.noreply.github.com>
2021-03-05 20:20:38 +0100

    Update AboutUs.js

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -14,6 +14,7 @@ import LocationCityIcon from '@material-ui/icons/LocationCity';
 
 
 
+
 export default class AboutUs extends Component {
     render() {
         return (
@@ -32,33 +33,44 @@ export default class AboutUs extends Component {
          
                 <div className="">
                 <div className="card-body my-5">
-                    <h5 className="card-title ">Team Members</h5>
-                    <p className="card-text ">With supporting text be
-                    ng text below as a natural leng text below as a natural le
-                    low as a natural lead-in 
-                    With supporting text be ng text below as a natural leng text below as a natural le low as a natural lead-in ng text below as a natural le ng text below as a natural leto additional content.
-                    With supporting text be ng text below as a natural leng text below as a natural le low as a natural lead-in ng text below as a natural le ng text below as a natural leto additional content.
-                    ng text below as a natural le
-                    ng text below as a natural leto additional content.</p>
+                    <h5 className="card-title "  style={{"color":"#34666F"}}>LAPro Group</h5>
+                    <p className="card-text ">We are LAPro group which is a group of 4 master students from all over the word who wants to success in his/her career and gain more experiences in computer science. Each of us has their own strengths. Baohui likes to design and searching in internet. She makes us always uptodated. Amr likes programming and likes to spend much time on machine learning algorithm and modify the attributes and get different results. Tannaz loves designing and coding, she looks always toward an userfriendly interface. Hesam is Network Profi and will solve all the problems with deploying and connections. 
+                    
+                     </p>
+                     <h6  style={{"color":"#34666F"}}>
+                     We work together with great passion and perseverance.
+                    We offer with talents in the following fields: Web design, back-end development, front-end development.</h6>
                 </div>
                 </div>
-
                        
-            <div className="d-flex container justify-content-center1" style={{"height":"200px"}}>
+            <div className="d-flex container justify-content-center1" style={{"height":"350px"}}>
                     <div className=" mx-5 mt-3" style={{"padding-top":"30px"}}>
                       <GroupIcon  style={{ fontSize: 65,fill: "white" }}></GroupIcon>
                     {/* <img src={aboutus} height="65px"/> */}
                     <p className="ptitle text-white">WHO WE ARE</p>
+
+                    <p className="ptitle text-white">Baohui Deng</p>
+                    <p className="ptitle text-white">Tannaz Vahidi</p>
+                    <p className="ptitle text-white">Amr Shakhshir</p>
+                    <p className="ptitle text-white">Hesam Heidarzadeh</p>
+
+
+
                         </div>  
                     <div className=" mx-5 mt-3" style={{"padding-top":"30px"}}>
                       <LocationCityIcon  style={{  fontSize: 65,fill: "white"  }}></LocationCityIcon>
                     {/* <img src={member} height="65px"/> */}
-                    <p className="ptitle text-white">WHERE WE AT</p>
+                    <p className="ptitle text-white">WHERE WE ARE</p>
+                    <p className="ptitle text-white">Duisburg-Essen University</p>
+                    <p className="ptitle text-white">Germany</p>
+
                         </div>  
                     <div className="mx-5 mt-3" style={{"padding-top":"30px"}}>
                       <HomeWorkIcon   style={{  fontSize: 65,fill: "white"  }}></HomeWorkIcon>
                     {/* <img src={aboutus} height="65px"/> */}
                     <p className="ptitle text-white">WHAT WE CAN DO</p>
+                    <p className="ptitle text-white">Programming</p>
+
                         </div>  
             </div> 
 
@@ -66,7 +78,7 @@ export default class AboutUs extends Component {
 
   <div className="container mt-5 ">
     <div className="boxes d-flex">
-    <div className="card border-info text-center mx-2 col-sm-6">
+    <div className="card border-info text-center mx-1 col-sm-6">
     <div className="card-body">
       <div className="box ">
         <img className="icon" src="https://img.icons8.com/plasticine/100/000000/html.png"/>
@@ -79,7 +91,7 @@ export default class AboutUs extends Component {
         Angular
       </div>
         <div className="feature">
-        React
+        React.js
       </div>
         <div className="feature">
          Bootstrap
@@ -88,16 +100,11 @@ export default class AboutUs extends Component {
       <div className="feature">
          Wordpress
       </div>
-      <div className="feature">
-         Git version control
-      </div>
-      <div className="feature">
-        Responsive layout and design
-      </div>
+
   </div>
   </div>
     </div>
-    <div className="card border-info text-center col-sm-6 mx-2">
+    <div className="card border-info text-center col-sm-6 mr-4">
     <div className="card-body">
     <div className="box">
           <img className="icon" style={{"max-width":"100%","height":"auto"}} src="https://img.icons8.com/bubbles/100/000000/database.png"/>
@@ -112,13 +119,7 @@ export default class AboutUs extends Component {
          NodeJS
         </div>
         <div className="feature">
-     C++
-        </div>
-        <div className="feature">
-     MySQL
-        </div>
-        <div className="feature">
-       MongoDB
+     PHP
         </div>
       </div>
   </div>
